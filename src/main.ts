c04970c2a58a323cd1835010e6b6a256e9fcc555
ekawa <onechuan2020@163.com>
2024-12-05 00:05:32 +0800

    feat: add error handler

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.config.errorHandler = (err, vm, info) => {
+  console.error(err, vm, info)
+}
+app.mount('#app')
