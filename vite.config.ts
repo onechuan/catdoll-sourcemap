cee4ca061db9bc31b53419332d191984ea3b5655
ekawa <onechuan2020@163.com>
2024-12-04 23:58:20 +0800

    feat: rollup add build

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,19 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base:"./",
   plugins: [vue()],
+  
   server: {
     host: '0.0.0.0',
     port: 9001
+  },
+  build:{
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue']
+        }
+      }
+    }
   }
 })
