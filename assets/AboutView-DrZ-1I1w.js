68307a948180f7d7aca0fba091421b00661e460e
onechuan <onechuan@users.noreply.github.com>
2024-12-08 16:58:19 +0000

    deploy: 10499894d4387e46bf2fb439dbfe33bdbcc4442b

---
+++ assets/AboutView-DrZ-1I1w.js
@@ -0,0 +1,2 @@
+import{_ as a,c as o,a as s,o as t}from"./index-D_0nUcts.js";const c={},n={class:"about"};function r(_,e){return t(),o("div",n,e[0]||(e[0]=[s("h1",null,"This is an about page",-1)]))}const i=a(c,[["render",r],["__scopeId","data-v-ad2dcbbc"]]);export{i as default};
+//# sourceMappingURL=AboutView-DrZ-1I1w.js.map
---
+++ assets/HomeView-BiPD5TnX.js
@@ -0,0 +1,2 @@
+import{_ as e,c,o as a}from"./index-D_0nUcts.js";const o={};function n(s,t){return a(),c("h1",null," This is an home page ")}const _=e(o,[["render",n],["__scopeId","data-v-631aad9c"]]);export{_ as default};
+//# sourceMappingURL=HomeView-BiPD5TnX.js.map
